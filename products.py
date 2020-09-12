48e88282e588cf2248c925ba7a01afbbf49c23ef
Kawa Araki <qq86853130pp@gmail.com>
2020-09-13 01:19:18 +0800

    new

--- products.py
+++ products.py
@@ -16,3 +16,8 @@ print(products[0][0])
 
 for p in products:
 	print(p[0],"的價格是",p[1])
+
+with open("products.txt","w", encoding="utf-8") as f:    # products.csv 可用excel打開
+	f.wite("商品" , "價格", "\n")
+	for p in products:
+		f.write(p[0] + "," + p[1] + "\n")
